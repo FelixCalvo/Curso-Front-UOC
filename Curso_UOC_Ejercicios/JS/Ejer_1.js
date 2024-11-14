f58bb10315a9eb3d275a0c4543689e5429bda14f
Félix <71280649+FelixCalvo@users.noreply.github.com>
2024-11-14 18:40:34 +0100

    res

--- Curso_UOC_Ejercicios/JS/Ejer_1.js
+++ Curso_UOC_Ejercicios/JS/Ejer_1.js
@@ -1,22 +1,19 @@
-const euros = 7;
-const dolares = euros * 2;// Modificar sólo esta línea
-console.log(dolares); // Debería mostrar 14
+const a = 3;
+const b = 4;
+let solucion = 0;
+const operacion = '9'; // +  -  *  /
 
-const precio = 100;
-const precioConIva = precio * 1.21// Modificar sólo esta línea
-console.log(precioConIva); // Debería mostrar 121
+if(operacion === '+'){
+  solucion = a + b;
+}else if(operacion === '-'){
+  solucion = a - b;
+}else if(operacion === '*'){
+  solucion = a * b;
+}else if(operacion === '/'){
+  solucion = a / b;
+}else{
+  solucion = "error";
+}
 
-const ancho=4;
-const alto=7;
-const area = ancho * alto;
-console.log(area)
-
-const centigrados = 20;
-const fahrenheit = (centigrados * 9 / 5) + 32;// Modificar sólo esta línea
-console.log(fahrenheit); // Debería mostrar 68
-
-const nombre = 'Juan';
-const material = 'madera';
-const dimensiones = 'diminutas';
-const comentario = 'Que sea bonita, pero de una belleza estraña, indómita';
-console.log(nombre + 'ha pedido una caja de '+ material + 'con unas dimensiones ' + dimensiones +'.' +comentario);
+console.log(solucion);
+  
