18ebdc234b6b8b93ca6fb605043b2bccdc4e4ff5
Félix <71280649+FelixCalvo@users.noreply.github.com>
2024-11-14 12:14:48 +0100

    Inicio JS

---
+++ Curso_UOC_Ejercicios/JS/Ejer_1.js
@@ -0,0 +1,32 @@
+//Ejer_1
+let entero = 10;
+console.log('Ejer_1: '+ entero)
+//Ejer_2
+let texto = 'Ejer_2_texto';
+console.log(texto)
+//Ejer_3
+let a;
+a = 7;
+let b = a;
+console.log('Ejer_3: ' + b)
+//Ejer_4
+let TituloDeLaPelicula;
+let camelCaseBienPuesto;
+let EDaddelMonstruo;
+let tituloDeLaPelicula;
+let edadDelMonstruo;
+//Ejer_5
+let i = 7;
+i = 5;
+i = 3;
+console.log(i); // ¿Qué valor se mostrará por consola?
+
+//Ejer_6
+let t = 5;
+let s = 7;
+let c = t;
+t = s;
+s = c;
+// ... aquí y sólo aquí añadiremos las líneas de codigo
+console.log('t: ', t); //Debería mostrar 7
+console.log('s: ', s); //Debería mostrar 5
