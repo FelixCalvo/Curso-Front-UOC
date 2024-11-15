1ea7b7df31011593986989767f75ec62c0fb58f2
Félix <71280649+FelixCalvo@users.noreply.github.com>
2024-11-15 19:59:40 +0100

    JS_grid

--- Curso_UOC_Ejercicios/JS/Ejer_1.js
+++ Curso_UOC_Ejercicios/JS/Ejer_1.js
@@ -1,19 +1,51 @@
-const a = 3;
-const b = 4;
-let solucion = 0;
-const operacion = '9'; // +  -  *  /
+// for(let x=1; x<101; x++){
+//   if(x%2===0) console.log(x);
+// }
 
-if(operacion === '+'){
-  solucion = a + b;
-}else if(operacion === '-'){
-  solucion = a - b;
-}else if(operacion === '*'){
-  solucion = a * b;
-}else if(operacion === '/'){
-  solucion = a / b;
-}else{
-  solucion = "error";
-}
 
-console.log(solucion);
-  
+// for(let x=1; x<101; x++){
+//   console.log(x%2===0 ? x : continue);
+// }
+
+
+// for(x=1; x<21; x++){
+//   if(x%3===0 && x%5===0){
+//     console.log(x + ' FizzBuzz');
+//   }else if(x%3===0){
+//     console.log(x + ' Fizz');
+//   }else if(x%5===0){
+//     console.log(x + ' Buz');
+//   }
+// }
+
+// let total = 0;
+// for (x=5; x>=0; x--){
+//   total = total + x;
+//   console.log(x + ' total --> ' + total)
+// }
+
+// for (let x = 1; x<=10; x++){
+//   console.log('7 * ' + x + ' = ' + x*7);
+// }
+// for(let y = 1; y <=10; y++){
+// for(let x = 1; x<=10; x++){
+//   console.log(y + ' * ' + x + ' = ' + x*y);  
+// }
+// };
+
+// let num = 5;
+// let total = 0;
+// while(num>=0){
+//   total+=num;
+//   console.log(num + ' total ' + total);
+//   num--;
+// }
+
+// let myVar = 87;
+// myVar++; // Modificar esta línea
+// console.log(myVar); // 88
+
+// let myVar = 11;
+// myVar--; // Modificar esta línea
+// console.log(myVar); // 10
+
